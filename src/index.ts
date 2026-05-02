53f0a1978ac94bad43213421c49b13d7095992d5
Gustavo Sanchez Galarza <gustavosanchezgalarza@gmail.com>
2026-05-02 12:38:24 -0700

    Add stream response

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,26 @@ import { mastra } from "./mastra";
 
 const chefGustavo = mastra.getAgent("chefGustavo");
 
-const query = "I am in Potrero Hill, which sushi restaurant do you recommend?";
+const query = "I am in SOMA, which sushi restaurant do you recommend?";
 console.log(`Query:  ${query}`);
 
-const limitedResult = await chefGustavo.generate(query, {
-  modelSettings: {
-    maxOutputTokens: 250,
-    temperature: 0.7,
-  },
+// Response text
+// const limitedResult = await chefGustavo.generate(query, {
+//   modelSettings: {
+//     maxOutputTokens: 300
+//   },
+// })
+// 
+// console.log(`Response of Chef Gustavo \n:`, limitedResult.text)
+
+// Response stream
+const stream = await chefGustavo.stream(query, { 
+    modelSettings: {
+        maxOutputTokens:300
+    }
 })
 
-console.log(`Response of Chef Gustavo \n:`, limitedResult.text)
+for await (const chunk of stream.textStream) {
+    process.stdout.write(chunk)
+}
+console.log("\n\n Response complete!!")
